2333c3fea96174f39df7cb129eb4780900951143
Bohdan Tereschenkov <jonieblack1488@gmail.com>
2025-05-08 17:21:04 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+def copy_file(command: str) -> None:
+    try:
+        if not command:
+            return
+        split_string = command.split()
+
+        if len(split_string) != 3 or split_string[0] != "cp":
+            return
+        origin, rep = split_string[1], split_string[2]
+
+        if origin == rep:
+            return
+
+        with open(origin, "r") as ori, open(rep, "w") as rep:
+            rep.write(ori.read())
+    except FileNotFoundError:
+        pass
